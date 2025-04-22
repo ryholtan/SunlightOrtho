fb04541be44c16efb38a5916a5c9b89493513367
Ryan Holt <ryholtan51@gmail.com>
2025-04-21 22:13:52 -0600

    added learn more page with images and videos

--- learnMore/learn.js
+++
@@ -1,8 +0,0 @@
-    // JavaScript for mobile menu toggle
-document.addEventListener('DOMContentLoaded', function () {
-        const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav-menu');
-        toggle.addEventListener('click', () => {
-    nav.classList.toggle('show');
-        });
-});
